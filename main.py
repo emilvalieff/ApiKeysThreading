ba6bc0cbba29a3aa25039247b4a0e6835461577c
Emil Valiyev <emilvalieff@gmail.com>
2025-03-08 01:08:42 +0300

    Func  added by Emil

---
+++ main.py
@@ -0,0 +1,28 @@
+import requests
+
+
+def get_crypto_data():
+    response = requests.get("https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")
+    if response.status_code == 200:
+        return response.json()
+
+
+crypto_response = get_crypto_data()
+user_input = input("Enter Your Cryto Currency: ")
+
+x = 0
+for crytpo in crypto_response:
+    x += 1
+    print(x)
+
+    if crytpo["currency"] == user_input:
+        print(crytpo["price"])
+        break
+
+
+
+
+
+
+#"https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json"
+
